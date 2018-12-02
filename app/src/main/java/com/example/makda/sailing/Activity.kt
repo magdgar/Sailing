371ea2ea68719ee1def0b22d4f29dc06e2e9109a
magdgar <magdgar@gmail.com>
2018-12-02 16:55:50 +0100

    Read json file from resources

--- app/src/main/java/com/example/makda/sailing/Activity.kt
+++ app/src/main/java/com/example/makda/sailing/Activity.kt
@@ -1,10 +1,13 @@
 package com.example.makda.sailing
 
+import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     val res = listOf("button1", "button3", "button1", "button1", "button2", "button4")
@@ -14,7 +17,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         when (buttonId) {
             res[times.rem(res.size)] -> Toast.makeText(this, "Won!!!", Toast.LENGTH_SHORT).show()
-            else -> Toast.makeText(this, "Button clicked! $buttonId", Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(this, readFileAsTestFromRes(), Toast.LENGTH_SHORT).show()
         }
         times++
     }
@@ -28,4 +31,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button3.setOnClickListener(this)
         button4.setOnClickListener(this)
     }
+
+    @SuppressLint("ResourceType")
+    fun readFileAsTestFromRes()
+                = resources.openRawResource(R.raw.questions).readBytes().toString(Charsets.UTF_8)
 }
