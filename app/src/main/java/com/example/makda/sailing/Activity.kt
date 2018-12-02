5b6072d106e4b66725a6cba79cb2d1d4e736d1f7
magdgar <magdgar@gmail.com>
2018-12-02 17:57:31 +0100

    Reas questions from json as JsonArray

--- app/src/main/java/com/example/makda/sailing/Activity.kt
+++ app/src/main/java/com/example/makda/sailing/Activity.kt
@@ -1,13 +1,13 @@
 package com.example.makda.sailing
 
-import android.annotation.SuppressLint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.view.View
 import android.widget.Toast
+import com.beust.klaxon.JsonArray
+import com.beust.klaxon.JsonObject
+import com.beust.klaxon.Parser
 import kotlinx.android.synthetic.main.activity_main.*
-import java.io.File
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     val res = listOf("button1", "button3", "button1", "button1", "button2", "button4")
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         when (buttonId) {
             res[times.rem(res.size)] -> Toast.makeText(this, "Won!!!", Toast.LENGTH_SHORT).show()
-            else -> Toast.makeText(this, readFileAsTestFromRes(), Toast.LENGTH_SHORT).show()
+            else -> Toast.makeText(this, "Button clicked! $buttonId", Toast.LENGTH_SHORT).show()
         }
         times++
     }
@@ -26,13 +26,19 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val questionsList = parse("/res/raw/q.json") as JsonArray<JsonObject>
+
+        Toast.makeText(this,  questionsList[0]["question"].toString(), Toast.LENGTH_SHORT).show()
         button1.setOnClickListener(this)
         button2.setOnClickListener(this)
         button3.setOnClickListener(this)
         button4.setOnClickListener(this)
     }
 
-    @SuppressLint("ResourceType")
-    fun readFileAsTestFromRes()
-                = resources.openRawResource(R.raw.questions).readBytes().toString(Charsets.UTF_8)
+    fun parse(name: String) : Any? {
+        val cls = Parser::class.java
+        return cls.getResourceAsStream(name)?.let { inputStream ->
+            return Parser.default().parse(inputStream)
+        }
+    }
 }
