ca5c495e120fdc65cf4fbf5ecc9aa9402c79f677
magdgar <magdgar@gmail.com>
2018-12-07 23:37:31 +0100

    Make Klaxon parsing json as Question object

--- app/src/main/java/com/example/makda/sailing/Activity.kt
+++ app/src/main/java/com/example/makda/sailing/Activity.kt
@@ -5,14 +5,32 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.beust.klaxon.JsonArray
-import com.beust.klaxon.JsonObject
+import com.beust.klaxon.JsonReader
+import com.beust.klaxon.Klaxon
 import com.beust.klaxon.Parser
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.StringReader
 import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     val res = listOf("button1", "button3", "button1", "button1", "button2", "button4")
-    private val questionsList = parse("/res/raw/q.json") as JsonArray<JsonObject>
+    private val questionsList = Klaxon().parseArray<Question>("[\n" +
+            "  {\n" +
+            "    \"question\": \"There is the questions?\",\n" +
+            "    \"answer\": 1,\n" +
+            "    \"images\": [\"R.drawable.sample_2\", \"R.drawable.sample_3\", \"R.drawable.sample_4\", \"R.drawable.sample_5\"]\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"question\": \"Whats your name?\",\n" +
+            "    \"answer\": 2,\n" +
+            "    \"images\": []\n" +
+            "  },\n" +
+            "  {\n" +
+            "    \"question\": \"Yet another question?\",\n" +
+            "    \"answer\": 1,\n" +
+            "    \"images\": []\n" +
+            "  }\n" +
+            "]")
     var questionNumber = 0
     var times = 0
 
@@ -20,7 +38,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val buttonId = v!!.resources.getResourceName(v.id).split("/button")[1]
 
         when (buttonId.toInt() - 1) {
-            questionsList[questionNumber]["answer"] -> Toast.makeText(this, "Won!!!", Toast.LENGTH_SHORT).show()
+            questionsList?.get(questionNumber)?.answer -> Toast.makeText(this, "Won!!!", Toast.LENGTH_SHORT).show()
             else -> Toast.makeText(this, "Button clicked! $buttonId", Toast.LENGTH_SHORT).show()
         }
         times++
@@ -29,10 +47,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        questionNumber = Random().nextInt(questionsList.size)
-        textView.text =  questionsList[questionNumber]["question"].toString()
-
-        button1.setImageResource(R.drawable.sample_2)
+        questionNumber = Random().nextInt(questionsList?.size!!)
+        textView.text = questionsList?.get(questionNumber)?.question ?: button1.setImageResource(R.drawable.sample_2 ).toString()
         button1.setOnClickListener(this)
         button2.setOnClickListener(this)
         button2.setImageResource(R.drawable.sample_2)
@@ -44,6 +60,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         val cls = Parser::class.java
         return cls.getResourceAsStream(name)?.let { inputStream ->
             return Parser.default().parse(inputStream)
+        }
+    }
+
+    fun streamingArray(json: String) {
+        val klaxon = Klaxon()
+        JsonReader(StringReader(json)).use { reader ->
+            val result = arrayListOf<Question>()
+            reader.beginArray {
+                while (reader.hasNext()) {
+                    val person = klaxon.parse<Question>(reader)
+                    if (person != null) {
+                        result.add(person)
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/example/makda/sailing/Question.kt
+++ app/src/main/java/com/example/makda/sailing/Question.kt
@@ -1,4 +1,6 @@
 package com.example.makda.sailing
 
+import com.beust.klaxon.Json
+
 data class QuestionsList(val version: Double, val questionsList: List<Question>)
-data class Question(val question: String, val answer: Int)
+data class Question(val question: String, val answer: Int, val images: List<Any>)
