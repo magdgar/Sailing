90ce4d8942b27a400d1d20eb0413a0b6733169e5
magdgar <magdgar@gmail.com>
2018-12-09 19:43:21 +0100

    Load button images specified in questions json

--- app/src/main/java/com/example/makda/sailing/Activity.kt
+++ app/src/main/java/com/example/makda/sailing/Activity.kt
@@ -1,17 +1,14 @@
 package com.example.makda.sailing
 
+import android.graphics.drawable.Drawable
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import com.beust.klaxon.JsonArray
-import com.beust.klaxon.JsonReader
 import com.beust.klaxon.Klaxon
 import com.beust.klaxon.Parser
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.StringReader
 import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -19,7 +16,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     val reso = readResource("/res/raw/q.json")
     private val questionsList = reso?.let { Klaxon().parseArray<Question>(it) }
     var questionNumber = 0
-    var times = 0
 
     override fun onClick(v: View?) {
         val buttonId = v!!.resources.getResourceName(v.id).split("/button")[1]
@@ -28,31 +24,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             questionsList?.get(questionNumber)?.answer -> Toast.makeText(this, "Won!!!", Toast.LENGTH_SHORT).show()
             else -> Toast.makeText(this, "Button clicked! $buttonId", Toast.LENGTH_SHORT).show()
         }
-        times++
+        loadNextQuestion()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        questionNumber = Random().nextInt(questionsList?.size!!)
-        textView.text = questionsList?.get(questionNumber).question
-        button1.setOnClickListener(this)
-        button2.setOnClickListener(this)
-        button2.setImageResource(R.drawable.sample_2)
-        button3.setOnClickListener(this)
-        button4.setOnClickListener(this)
+        loadNextQuestion()
     }
 
-    fun readResource(name: String) : String? {
+    private fun loadNextQuestion() {
+        questionNumber = Random().nextInt(questionsList?.size!!)
+        val question =  questionsList?.get(questionNumber)
+        setButtonImages(question)
+        setButtonsOnClickListeners()
+        textView.text = question.question
+    }
+
+    private fun readResource(name: String) : String? {
         val cls = Parser::class.java
         val outputStream = ByteArrayOutputStream()
         return cls.getResourceAsStream(name)?.let { inputStream ->
             inputStream.use {
-                input -> ByteArrayOutputStream().use {
-                output -> input.copyTo(outputStream)
+                    input -> ByteArrayOutputStream().use {
+                    _ -> input.copyTo(outputStream)
             }
             }
             return String(outputStream.toByteArray(), Charsets.UTF_8)
         }
+    }
+
+    private fun setButtonImages(question: Question) {
+        button1.setImageResource(IMAGES.valueOf(question.images[0]).id)
+        button2.setImageResource(IMAGES.valueOf(question.images[1]).id)
+        button3.setImageResource(IMAGES.valueOf(question.images[2]).id)
+        button4.setImageResource(IMAGES.valueOf(question.images[3]).id)
+    }
+
+    private fun setButtonsOnClickListeners(){
+        button1.setOnClickListener(this)
+        button2.setOnClickListener(this)
+        button3.setOnClickListener(this)
+        button4.setOnClickListener(this)
     }
 }
--- app/src/main/java/com/example/makda/sailing/Question.kt
+++ app/src/main/java/com/example/makda/sailing/Question.kt
@@ -1,6 +1,22 @@
 package com.example.makda.sailing
 
-import com.beust.klaxon.Json
-
 data class QuestionsList(val version: Double, val questionsList: List<Question>)
-data class Question(val question: String, val answer: Int, val images: List<Any>)
+data class Question(val question: String, val answer: Int, val images: List<String>)
+
+//enum class IMAGES {
+//    IMG_TWO {
+//        override fun id(): Int = R.drawable.sample_2
+//    },
+//
+//    IMG_THREE {
+//        override fun id(): Int =
+//    };
+//    abstract fun id(): Int
+//}
+
+enum class IMAGES (val id: Int){
+    IMG_TWO(R.drawable.sample_2),
+    IMG_THREE(R.drawable.sample_3),
+    IMG_FOUR(R.drawable.sample_4),
+    IMG_FIVE(R.drawable.sample_5)
+}
