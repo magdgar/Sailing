ddf4bd198f0de4e0bf724630f898bda4b92461bc
magdgar <magdgar@gmail.com>
2018-12-09 17:20:11 +0100

    Read questions from json file

--- app/src/main/java/com/example/makda/sailing/Activity.kt
+++ app/src/main/java/com/example/makda/sailing/Activity.kt
@@ -9,28 +9,15 @@ import com.beust.klaxon.JsonReader
 import com.beust.klaxon.Klaxon
 import com.beust.klaxon.Parser
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.ByteArrayOutputStream
+import java.io.File
 import java.io.StringReader
 import java.util.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     val res = listOf("button1", "button3", "button1", "button1", "button2", "button4")
-    private val questionsList = Klaxon().parseArray<Question>("[\n" +
-            "  {\n" +
-            "    \"question\": \"There is the questions?\",\n" +
-            "    \"answer\": 1,\n" +
-            "    \"images\": [\"R.drawable.sample_2\", \"R.drawable.sample_3\", \"R.drawable.sample_4\", \"R.drawable.sample_5\"]\n" +
-            "  },\n" +
-            "  {\n" +
-            "    \"question\": \"Whats your name?\",\n" +
-            "    \"answer\": 2,\n" +
-            "    \"images\": []\n" +
-            "  },\n" +
-            "  {\n" +
-            "    \"question\": \"Yet another question?\",\n" +
-            "    \"answer\": 1,\n" +
-            "    \"images\": []\n" +
-            "  }\n" +
-            "]")
+    val reso = readResource("/res/raw/q.json")
+    private val questionsList = reso?.let { Klaxon().parseArray<Question>(it) }
     var questionNumber = 0
     var times = 0
 
@@ -48,7 +35,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         questionNumber = Random().nextInt(questionsList?.size!!)
-        textView.text = questionsList?.get(questionNumber)?.question ?: button1.setImageResource(R.drawable.sample_2 ).toString()
+        textView.text = questionsList?.get(questionNumber).question
         button1.setOnClickListener(this)
         button2.setOnClickListener(this)
         button2.setImageResource(R.drawable.sample_2)
@@ -56,25 +43,16 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button4.setOnClickListener(this)
     }
 
-    fun parse(name: String) : Any? {
+    fun readResource(name: String) : String? {
         val cls = Parser::class.java
+        val outputStream = ByteArrayOutputStream()
         return cls.getResourceAsStream(name)?.let { inputStream ->
-            return Parser.default().parse(inputStream)
-        }
-    }
-
-    fun streamingArray(json: String) {
-        val klaxon = Klaxon()
-        JsonReader(StringReader(json)).use { reader ->
-            val result = arrayListOf<Question>()
-            reader.beginArray {
-                while (reader.hasNext()) {
-                    val person = klaxon.parse<Question>(reader)
-                    if (person != null) {
-                        result.add(person)
-                    }
-                }
+            inputStream.use {
+                input -> ByteArrayOutputStream().use {
+                output -> input.copyTo(outputStream)
             }
+            }
+            return String(outputStream.toByteArray(), Charsets.UTF_8)
         }
     }
 }
