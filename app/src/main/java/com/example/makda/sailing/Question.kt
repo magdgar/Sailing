c14792ba3c5db089d9f02eb6393d437b7c6c8a78
magdgar <magdgar@gmail.com>
2018-12-14 23:58:32 +0100

    First sailing question

--- app/src/main/java/com/example/makda/sailing/Question.kt
+++ app/src/main/java/com/example/makda/sailing/Question.kt
@@ -6,5 +6,9 @@ enum class IMAGES (val id: Int){
     IMG_TWO(R.drawable.sample_2),
     IMG_THREE(R.drawable.sample_3),
     IMG_FOUR(R.drawable.sample_4),
-    IMG_FIVE(R.drawable.sample_5)
+    IMG_FIVE(R.drawable.sample_5),
+    BERTH_BAN(R.drawable.zakaz_cumowania),
+    ANCHOR_BAN(R.drawable.zakaz_kotwiczenia),
+    PASS_BAN(R.drawable.zakaz_mijania),
+    PRONATION_BAN(R.drawable.zakaz_nawracania)
 }
